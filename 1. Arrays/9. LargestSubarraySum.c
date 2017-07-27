f1a2e9142776867828540dec14968ff12ae738d4
Nikita Rai <nikrc15@gmail.com>
2017-07-27 17:27:30 +0530

    Update 9. LargestSubarraySum.c

--- 1. Arrays/9. LargestSubarraySum.c
+++ 1. Arrays/9. LargestSubarraySum.c
@@ -1,16 +1,23 @@
 #include<stdio.h>
 
 int findLargestSum(int arr[], int n){
-    int max_till_now=0, max_end=0, i;
+    int max_till_now=0, max_end=0, i, start=0, end=0, s=0;
 
     for(i=0;i<n;i++){
         max_end += arr[i];
 
-        if(max_till_now < max_end)
+        if(max_till_now < max_end){
             max_till_now = max_end;
-        if(max_end < 0)
+            start = s;
+            end = i;
+        }
+        if(max_end < 0){
             max_end = 0;
+            s = i+1;
+        }
     }
+    printf("\nStarting index: %d", start);
+    printf("\nLast index: %d", end);
     return max_till_now;
 }
 
